b78c29a34acff1ab9984fb80f6edb3fc04acaeb7
Anas Harby <anas.harby@gmail.com>
2018-01-24 14:03:21 +0200

    Support bluetooth connection

---
+++ app/src/main/java/com/example/notify/arduino/androidnotificationlistener/ArduinoListenerService.java
@@ -0,0 +1,113 @@
+package com.example.notify.arduino.androidnotificationlistener;
+
+import android.app.Notification;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.service.notification.NotificationListenerService;
+import android.service.notification.StatusBarNotification;
+import android.util.Log;
+
+import com.example.notify.arduino.bluetoothcontrol.BluetoothConnection;
+
+/**
+ * Created by mohammed on 1/23/18.
+ */
+
+public class ArduinoListenerService extends NotificationListenerService {
+    private String TAG = this.getClass().getSimpleName();
+
+    protected static final String NOTIFICATION_POSTED_ACTION =
+            "com.example.notify.arduino.androidnotificationlistener" +
+                    ".NOTIFICATION_LISTENER_POSTED_ACTION";
+
+    protected static final String NOTIFICATION_REMOVED_ACTION =
+            "com.example.notify.arduino.androidnotificationlistener" +
+                    ".NOTIFICATION_LISTENER_REMOVED_ACTION";
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return super.onBind(intent);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(NOTIFICATION_POSTED_ACTION);
+        filter.addAction(NOTIFICATION_REMOVED_ACTION);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    private String getTickerText(Notification notification) {
+        final String TITLE_KEY = "android.title";
+        final String TEXT_KEY = "android.text";
+        String ticker = "";
+
+        if (notification != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                try {
+                    Bundle extras = notification.extras;
+                    String extraTitle = extras.getString(TITLE_KEY);
+                    String extraText = null;
+                    Object extraTextExtra = extras.get(TEXT_KEY);
+                    if (extraTextExtra != null) extraText = extraTextExtra.toString();
+
+                    if (extraTitle != null && extraText != null && !extraText.isEmpty()) {
+                        ticker = extraTitle + ": " + extraText;
+                    } else if (extraTitle != null) {
+                        ticker = extraTitle;
+                    } else if (extraText != null) {
+                        ticker = extraText;
+                    }
+                } catch (Exception e) {
+                    Log.w("NotificationPlugin", "problem parsing notification extras for " + notification.tickerText);
+                    e.printStackTrace();
+                }
+            }
+
+            if (ticker.isEmpty()) {
+                ticker = (notification.tickerText != null) ? notification.tickerText.toString() : "";
+            }
+        }
+
+        return ticker;
+    }
+
+    @Override
+    public void onNotificationPosted(StatusBarNotification sbn) {
+
+        Log.i(TAG,"**********onNotificationPosted**********");
+        Log.i(TAG,"ID :" + sbn.getId() + "\t" + getTickerText(sbn.getNotification())
+                + "\t" + sbn.getPackageName());
+
+        Intent intent = new Intent(NOTIFICATION_POSTED_ACTION);
+        intent.putExtra("package_name", sbn.getPackageName());
+        intent.putExtra("notification_text", getTickerText(sbn.getNotification()));
+        //intent.putExtra("notification_icon", sbn.getNotification().getSmallIcon());
+        intent.putExtra("status_bar_notification", sbn);
+        sendBroadcast(intent);
+    }
+
+    @Override
+    public void onNotificationRemoved(StatusBarNotification sbn) {
+        Log.i(TAG,"**********onNotificationRemoved**********");
+        Log.i(TAG,"ID :" + sbn.getId() + "\t" + getTickerText(sbn.getNotification())
+                +"\t" + sbn.getPackageName());
+
+        Intent intent = new Intent(NOTIFICATION_REMOVED_ACTION);
+        intent.putExtra("package_name", sbn.getPackageName());
+        intent.putExtra("notification_text", getTickerText(sbn.getNotification()));
+        //intent.putExtra("notification_icon", sbn.getNotification().getSmallIcon());
+        intent.putExtra("status_bar_notification", sbn);
+        sendBroadcast(intent);
+    }
+
+}
---
+++ app/src/main/java/com/example/notify/arduino/androidnotificationlistener/MainActivity.java
@@ -0,0 +1,123 @@
+package com.example.notify.arduino.androidnotificationlistener;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.service.notification.StatusBarNotification;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.notify.arduino.bluetoothcontrol.BluetoothConnection;
+
+public class MainActivity extends AppCompatActivity {
+
+    /* HC-05 Bluetooth module MAC address. */
+    protected static final String HC05_MAC_ADDRESS = "00211300F3C4";
+
+    private String TAG = this.getClass().getSimpleName();
+    /**
+     * A TextView for testing.
+     */
+    private TextView txtView;
+
+    private Button connectButton;
+
+    private NotificationReceiver notificationReceiver;
+
+    private BluetoothConnection btConnection;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtView = (TextView) findViewById(R.id.textView);
+        connectButton = (Button) findViewById(R.id.connectButton);
+        notificationReceiver = new NotificationReceiver();
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(ArduinoListenerService.NOTIFICATION_POSTED_ACTION);
+        filter.addAction(ArduinoListenerService.NOTIFICATION_REMOVED_ACTION);
+        registerReceiver(notificationReceiver, filter);
+
+        connectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btConnection = new BluetoothConnection(HC05_MAC_ADDRESS);
+                btConnection.execute();
+            }
+        });
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(notificationReceiver);
+//        stopService(new Intent(this, ArduinoListenerService.class));
+    }
+
+    class NotificationReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            switch (intent.getAction()) {
+                case ArduinoListenerService.NOTIFICATION_POSTED_ACTION:
+                    handleNotificationPosted(context, intent);
+                    break;
+                case ArduinoListenerService.NOTIFICATION_REMOVED_ACTION:
+                    handleNotificationRemoved(context, intent);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void handleNotificationPosted(Context context, Intent intent) {
+            Log.i(TAG, "**********handleNotificationPosted**********");
+            Log.i(TAG, "Posted: " + intent.getStringExtra("package_name")
+                    + ": " + intent.getStringExtra("notification_text"));
+
+            // TODO: use the sbn to get the icon
+            StatusBarNotification sbn = intent.getParcelableExtra("status_bar_notification");
+
+            txtView.setText(txtView.getText() + "\n" + "Posted: "
+                    + intent.getStringExtra("package_name") + ": "
+                    + intent.getStringExtra("notification_text"));
+
+            if (btConnection != null) {
+                btConnection.send("1");
+            } else {
+                Toast.makeText(getApplicationContext(), "Connect to HC-05 first.",
+                        Toast.LENGTH_LONG);
+            }
+        }
+
+        private void handleNotificationRemoved(Context context, Intent intent) {
+            Log.i(TAG, "**********handleNotificationRemoved**********");
+            Log.i(TAG, "Removed: " + intent.getStringExtra("package_name") + ": "
+                    + intent.getStringExtra("notification_text"));
+
+            // TODO: use the sbn to get the icon
+            StatusBarNotification sbn = intent.getParcelableExtra("status_bar_notification");
+
+            txtView.setText(txtView.getText() + "\n" + "Removed: "
+                    + intent.getStringExtra("package_name") + ": "
+                    + intent.getStringExtra("notification_text"));
+
+            if (btConnection != null) {
+                btConnection.send("0");
+            } else {
+                Toast.makeText(getApplicationContext(), "Connect to HC-05 first.",
+                        Toast.LENGTH_LONG);
+            }
+        }
+    }
+}
